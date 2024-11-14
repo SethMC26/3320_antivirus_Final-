6ee0bacea02ca297b2bf44aa9a1fc95ddd6569b6
SethMC26 <holtzmans@merrimack.edu>
2024-11-13 20:55:49 -0500

    Refactor: Added cleanup of log files to uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,4 +6,9 @@ echo "removing hash data from /usr/local/share/pproc/"
 # remove binary 
 sudo rm /usr/local/bin/pproc
 echo "Removed binary from /usr/local"
+
+#remove logs 
+sudo rm /var/log/pproc.log 
+sudo rm ~/pproc.log
+echo "Removed log files from root and user directories"
 echo "Successfully uninstalled program"
