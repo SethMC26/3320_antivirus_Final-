e31d67cd20c97eaa99611e2f3cb989aace6c8d3a
SethMC26 <holtzmans@merrimack.edu>
2024-11-17 16:11:48 -0500

    Doc: Added API docs

--- src/Utils/fileHandler.h
+++ src/Utils/fileHandler.h
@@ -1,9 +1,26 @@
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 
+/**
+ * Handles a possibly malicious file
+ * 
+ * @param target_file string of target file to handle
+ */
 int handle_malicious_file(const char* target_file);
-
+/**
+ * Check if a file is on the whitelist
+ * 
+ * @param target_file string of file to check
+ */
 int is_whitelisted(const char* target_file);
+
+/**
+ * Adds absolute path of file to white list 
+ * 
+ * @param target_file adds file to white list 
+ */
+int add_to_whitelist(const char* target_file);
+
 /**
  * Gets yes or no user input
  *
